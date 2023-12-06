08842eda906b1d4d131311192b39c015de498593
bgsdo1234 <victor270196@gmail.com>
2023-12-06 15:01:27 -0300

    textos ao lado da primeira imagem

--- src/app/page.js
+++ src/app/page.js
@@ -15,11 +15,34 @@ function Seta({x, y, z}) {
   );
 }
 
+function Trending({x, y}) {
+  return (
+    <h1 style={{left: x , top : y}} className={styles.trending}>TRENDING ON BOARD <span style={{left: x, top: y}} className={styles.comp}>#1</span></h1>
+  );
+}
+
+function Off({x, y}) {
+  return (
+    <h2 style={{left: x , top : y}} className={styles.off}>Off-the-Beaten-Path Destinations</h2>
+  );
+}
+
+function Embark({x, y}) {
+  return (
+    <h3 style={{left: x , top : y}} className={styles.embark}>Embark on a virtual journey to discover lesser-known travel destinations that offer unique cultural experiences.</h3>
+  );
+}
+
+
 export default function Home(){
   return (
     <>
     <Img x={67} y={295}/>
     <Seta x={909} y={317} z={58}/>
+    <Trending></Trending>
+    <Off></Off>
+    <Embark></Embark>
+    
     
     </>
   )
