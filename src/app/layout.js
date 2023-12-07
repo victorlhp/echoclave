d0c52591f63de4323d6f9a7d7c6d823120b4c350
Luanoly <75376776+roberto06rhz@users.noreply.github.com>
2023-12-07 10:05:51 -0300

    pull

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Image from 'next/image'
 import lupa from '../../public/lupa.svg'
-import styles from './page.module.css'
+import styles from './styles.module.css'
 
 function Titulo({x, y}) {
   return (
@@ -18,9 +18,24 @@ function BackTitulo({x, y}) {
 
 function Lupa({x, y}) {
   return (
-    <Image src={lupa} style={{left: x, top:y}} className={styles.lupa}/>
+    <Image src={lupa} style={{left: x, top: y}} className={styles.lupa} alt='Lupa'/>
   );
-  
+}
+
+function Menu({children, x, y}){
+  return(
+    <ul style={{left: x, top: y}} className={styles.menu}>
+      {children.map((itemMenu) => (
+        <li key={itemMenu.nome}>{itemMenu}</li>
+      ))}
+    </ul>
+  );
+}
+
+function ItemMenu({nome}){
+  return (
+    <p className={styles.itemMenu}>{nome}</p>
+  );
 }
 
 export default function RootLayout({ children }){
@@ -28,6 +43,18 @@ export default function RootLayout({ children }){
   <>
     <Titulo x={713} y={24}></Titulo>
     <BackTitulo x={1728} y={122}></BackTitulo>  
+      <Menu x={435} y={60}>
+              <ItemMenu nome="All"/>
+              <ItemMenu nome="Travel"/>
+              <ItemMenu nome="Technology"/>
+              <ItemMenu nome="Sports"/>
+              <ItemMenu nome="Literature"/>
+              <ItemMenu nome="Entertainment"/>
+              <ItemMenu nome="Art"/>
+              <ItemMenu nome="Business"/>
+              <ItemMenu nome="Health"/>
+              <ItemMenu nome="Climate"/>
+            </Menu>
     <Lupa x={1581} y={85}/>
     {children}
   </>
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,63 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import img1 from '../../public/img1.png'
+import seta1 from '../../public/seta1.png'
+import ellipse from '../../public/ellipse.svg'
 
-// function Titulo({x, y}) {
-//   return (
-//     <h1 style={{left: x , top : y}} className={styles.titulo}>EchoClave</h1>
-//   );
-// }
+function Img({x, y}) {
+  return (
+    <Image src={img1} style={{left: x, top: y}} className={styles.img} alt='img'/>
+  );
+}
 
-// function BackTitulo({x, y}) {
-//   return (
-//     <h1 style={{width: x , height : y}} className={styles.backTitulo}></h1>
-//   );
-// }
+function Seta({x, y, z}) {
+  return (
+    <Image src={seta1} style={{left: x, top: y, radius: z}} className={styles.seta} alt='seta'/>
+  );
+}
+
+function Trending({x, y}) {
+  return (
+    <h1 style={{left: x , top : y}} className={styles.trending}>TRENDING ON BOARD </h1>
+  );
+}
+
+function Comp({x, y}) {
+  return (
+    <h4 style={{left: x, top: y}} className={styles.comp}>#1</h4>
+  );
+}
+
+function Off({x, y}) {
+  return (
+    <h2 style={{left: x , top : y}} className={styles.off}>Off-the-Beaten-Path Destinations</h2>
+  );
+}
+
+function Embark({x, y}) {
+  return (
+    <h3 style={{left: x , top : y}} className={styles.embark}>Embark on a virtual journey to discover lesser-known travel destinations that offer unique cultural experiences.</h3>
+  );
+}
+
+function Ellipse({x, y}) {
+  return (
+    <Image src={ellipse} style={{left: x, top: y}} className={styles.ellipse} alt='ellipse'/>
+  );
+}
+
 
 export default function Home(){
   return (
     <>
-    {/* <Titulo>x={713} y={24}</Titulo>
-    <BackTitulo>x={1728} y={122}</BackTitulo> */}
+    <Img x={67} y={295}/>
+    <Seta x={909} y={317} z={58}/>
+    <Ellipse>x={1080} y={731}</Ellipse>
+    <Trending></Trending>
+    <Comp></Comp>
+    <Off></Off>
+    <Embark></Embark>
+    
     
     </>
   )
